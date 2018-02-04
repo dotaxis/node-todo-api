602cc57ec488554742e68806426da2e17855d748
Chase Taylor <x8@live.ca>
2018-02-04 11:52:05 -0700

    Added /GET route and test script

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,14 +5,33 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
+const dummyDB = [{
+    text: 'First test to do'
+}, {
+    text: 'Second test'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(dummyDB);
+    }).then(() => done());
+});
+
+describe('GET /todos', () => {
+    it('Should get all Todos from DB', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        }).
+        end(done());
+    });
 });
 
 describe('POST /todos', () => {
     it('Should add new Todo to DB', (done) => {
-        var text = 'Thing to do';
-
+        var text = 'New entry';
         request(app)
         .post('/todos')
         .send({text})
@@ -24,8 +43,8 @@ describe('POST /todos', () => {
             if (e) return done(e);
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(1);
-                expect(todos[0].text).toBe(text);
+                expect(todos.length).toBe(3);
+                expect(todos[2].text).toBe(text);
                 done();
             }).catch((e) => done(e))
         });
@@ -40,7 +59,7 @@ describe('POST /todos', () => {
             if (e) return done(e);
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
         });
