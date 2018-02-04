77a9b9b35dc736c892f09e64af75f9cd2521c24c
Chase Taylor <x8@live.ca>
2018-02-04 12:33:08 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -27,6 +29,27 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.stats(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({text: 'Not a valid ID.'});
+        return console.log('Invalid ID.');
+    }
+
+    User.findById(id).then((user) => {
+        if (!user) {
+            res.status(404).send({text: 'No user with that ID.'});
+            return console.log('No user with that ID.');
+        }
+        res.send(user);
+        console.log((`User by ID: ${JSON.stringify(user, null, 4)}`));
+    }).catch((e) => console.log(`[!] Exception: ${e}`));
+}, (e) => {
+    console.log(`[!] Exception: ${e}`);
+    res.status(400).send();
 });
 
 // GET /todos/123fbxjafkfb
